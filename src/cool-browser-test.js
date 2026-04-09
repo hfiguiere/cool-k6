83df09c41de127f9b476e83c1d5910f742fc8f37
Hubert Figuière <hub@collabora.com>
2026-04-09 17:36:39 -0400

    browser test: wait for postmessage event
    Now load the frame from an outter page.
    Loading the cool frame is done by filling
    the form and clikcing the button.
    
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- src/cool-browser-test.js
+++ src/cool-browser-test.js
@@ -9,7 +9,7 @@ import { sleep, check, fail } from 'k6';
 import { Trend } from 'k6/metrics';
 
 import { checkWopi, getWopiClientUrl, getWopiSrc } from '../lib/wopi_discovery.js';
-import { screenshotPage } from '../lib/test_utils.js';
+import { screenshotPage, setWopiClientAndFile, startCool, watchPostMessages, waitForMessage } from '../lib/test_utils.js';
 import { wopiHost, wopiUrl } from './config.js';
 
 export const options = {
@@ -42,26 +42,42 @@ export function setup() {
 }
 
 export default async function () {
-    let wopiClient = await getWopiClientUrl(wopiUrl);
-    let wopiSrc = getWopiSrc(wopiHost, 2);
-
-    let wopiClientUrl = new URL(wopiClient);
-    wopiClientUrl.searchParams.set("WOPISrc", wopiSrc);
-
     let context = await browser.newContext(browserOptions);
     const page = await context.newPage();
     try {
+        page.on('console', (msg) => {
+            let text = msg.text();
+            if (text.startsWith("DEBUG")) {
+                console.log(`page log: ${msg.type()} - ${text}`);
+            }
+        });
+
         let start = Date.now();
         frameLoadingTime.add(0);
         pageLoadingTime.add(0);
-        let resp = await page.goto(wopiClientUrl.toString());
+        await page.goto(wopiHost);
         pageLoadingTime.add(Date.now() - start);
 
-        const locator = page.locator('canvas#document-canvas');
-        await locator.waitFor({state: 'visible'});
+        await setWopiClientAndFile(page, wopiUrl.toString(), 2);
+        await watchPostMessages(page, [ "App_LoadingStatus" ]);
+        await startCool(page);
+
+        await screenshotPage(page);
+
+        let ready = false;
+        do {
+            let message = await waitForMessage(page, "App_LoadingStatus");
+            console.log(`message2: ${JSON.stringify(message)}`);
+            if (message) {
+                ready = (message.Values.Status == "Frame_Ready");
+            }
+        } while (!ready);
+        check(ready, {
+            "Got Frame Ready": ready => ready
+        });
         frameLoadingTime.add(Date.now() - start);
     } catch (error) {
-        screenshotPage(page);
+        await screenshotPage(page);
         fail(`Browser iteration failed: ${error}`);
     } finally {
         await page.close();
